b88ca5811d28fb80b6c63d8884deeca9d3640e41
rubuz <rubuz23@gmail.com>
2023-12-08 14:33:12 +0100

    style: dekstop navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,26 @@
 import React from "react";
+import { FaArrowRight } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav className="w-screen p-8">
-      <div className="max-w-[1400px] w-1/4 mx-auto">
-        <ul className="flex justify-between items-center">
-          <li className="bg-pinky rounded-full px-6 py-2">
+    <nav className="w-screen flex justify-center">
+      <div className="max-w-[1400px] w-1/4 ">
+        <ul className="flex justify-around items-center h-14 gap-x-8">
+          <li className="group nav-btn hover:bg-pinky transition-all duration-500 ease-in-out">
             <a href="">home</a>
+            <FaArrowRight className="nav-icon" />
           </li>
-          <li>
+          <li className="group nav-btn hover:bg-pinky transition-all duration-500 ease-in-out">
             <a href="">about</a>
+            <FaArrowRight className="nav-icon" />
           </li>
-          <li>
+          <li className="group nav-btn hover:bg-pinky transition-all duration-500 ease-in-out">
             <a href="">work</a>
+            <FaArrowRight className="nav-icon" />
           </li>
-          <li>
+          <li className="group nav-btn hover:bg-pinky transition-all duration-500 ease-in-out">
             <a href="">contact</a>
+            <FaArrowRight className="nav-icon" />
           </li>
         </ul>
       </div>
