d3f765fa2e7ec91e29c555207df6b61c9a2e6ad5
rubuz <rubuz23@gmail.com>
2023-12-15 12:33:12 +0100

    style: horizontal scroll done?

--- src/App.jsx
+++ src/App.jsx
@@ -10,24 +10,37 @@ function App() {
   const targetRef = useRef(null);
   const { scrollYProgress } = useScroll({
     target: targetRef,
+    offset: ["0.5 0.9", "1.5 1"],
   });
 
-  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-100%"]);
+  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-95%"]);
+  const scaleProgress = useTransform(scrollYProgress, [0, 0.4, 1], [1, 1, 0]);
+  const opacityProgress = useTransform(scrollYProgress, [0, 0.7, 1], [1, 0, 0]);
 
   return (
     <div className="font-work">
       <Navbar />
-      {/* <TitleChanger /> */}{" "}
-      <s ection ref={targetRef} className="relative h-[160vh] flex flex-col">
-        <div className="sticky -top-32">
+      <TitleChanger />
+      <section
+        ection
+        ref={targetRef}
+        className="relative h-[190vh] flex flex-col"
+      >
+        <motion.div
+          style={{
+            scale: scaleProgress,
+            opacity: opacityProgress,
+          }}
+          className=" sticky top-[-20%] h-[55vh]"
+        >
           <Hero />
-        </div>
-        <div className="sticky top-[55%]">
+        </motion.div>
+        <div className="sticky top-[30%]">
           <motion.div style={{ x }} className="flex gap-8 mb-10">
             <AboutCards />
           </motion.div>
-        </div>{" "}
-      </s>
+        </div>
+      </section>
       <div className="w-full h-[1900px] bg-purple-800">TEST</div>{" "}
     </div>
   );
