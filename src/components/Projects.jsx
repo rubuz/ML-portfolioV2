7f2957e51b9ccde13265371f6296cd002c0d4b31
rubuz <rubuz23@gmail.com>
2023-12-18 15:28:40 +0100

    style: dot hover effect

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,20 @@ import React from "react";
 import Project1 from "../assets/project1.png";
 
 const Projects = () => {
+  const projectDot = document.querySelector(".dot-container");
+  console.log(projectDot);
+  const handleHover = () => {
+    if (projectDot) {
+      projectDot.classList.add("hoverd");
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (projectDot) {
+      projectDot.classList.remove("hoverd");
+    }
+  };
+
   return (
     <section className="w-full  bg-greeny rounded-t-[2rem]">
       <div>
@@ -9,12 +23,16 @@ const Projects = () => {
       </div>
       <div className="w-[75%] mx-auto grid grid-cols-[60%_auto] mt-10 gap-10">
         <div className="w-full bg-transparent rounded-[2rem] border-[3px] p-9 border-black flex flex-col justify-between gap-12">
-          <div className="flex gap-2">
-            <div className="w-6 h-6 rounded-full border-[3px] border-black bg-transparent project-dot"></div>
-            <div className="w-6 h-6 rounded-full border-[3px] border-black bg-transparent"></div>
-            <div className="w-6 h-6 rounded-full border-[3px] border-black bg-transparent"></div>
+          <div className="flex gap-2 dot-container">
+            <div className="w-6 h-6 rounded-full border-[3px] border-black transition-all duration-500 project-dot"></div>
+            <div className="w-6 h-6 rounded-full border-[3px] border-black transition-all duration-500 project-dot"></div>
+            <div className="w-6 h-6 rounded-full border-[3px] border-black transition-all duration-500 project-dot"></div>
           </div>
-          <div className="w-full project-img">
+          <div
+            className="w-full project-img"
+            onMouseEnter={handleHover}
+            onMouseLeave={handleMouseLeave}
+          >
             <img
               src={Project1}
               alt=""
