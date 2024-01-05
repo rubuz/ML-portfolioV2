dd0aefb6f26ca56168b7c834c5043da5d632e56a
rubuz <rubuz23@gmail.com>
2024-01-05 14:52:25 +0100

    Add @headlessui/react package and implement dark mode switch

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,34 @@
 import { BsArrowRight } from "react-icons/bs";
+import { IoMdMoon, IoMdSunny } from "react-icons/io";
 import "./Hero.css";
 import Button from "./Button";
+import { Switch } from "@headlessui/react";
+import { useState } from "react";
 
 const Hero = () => {
+  const [enabled, setEnabled] = useState(false);
+
   return (
     <div className="mt-20 sm:max-w-[70%] max-w-[95%] mx-auto flex lg:flex-row-reverse flex-col-reverse justify-center items-center sm:gap-20 gap-10 lg:gap-10">
       <div className="mx-auto max-w-[90%] hero__img"></div>
       <div className="lg:w-1/2 w-4/5 flex flex-col items-start gap-8 mx-auto">
-        <button>theme</button>
+        <div>
+          <Switch
+            checked={enabled}
+            onChange={setEnabled}
+            className={`${enabled ? "bg-bluey" : "bg-greeny"}
+          relative inline-flex items-center h-[38px] w-[74px] shrink-0 cursor-pointer rounded-full border-2 border-black transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white/75`}
+          >
+            <span className="sr-only">Dark mode</span>
+            <IoMdMoon className="absolute h-[30px] w-[30px] left-0.5 text-white" />
+            <span
+              aria-hidden="true"
+              className={`${enabled ? "translate-x-9" : "translate-x-0"}
+            pointer-events-none inline-block h-[34px] max-w-[34px] z-10 transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+            />
+            <IoMdSunny className="absolute h-[30px] w-[30px] right-0.5 text-white" />
+          </Switch>
+        </div>
         <div className="relative">
           <h2 className="lg:text-3xl sm:text-4xl text-xl">
             Self-taught web developer with a passion for front-end based in
